2a397ae2f0059e4c117f2eaa875360d3ea49683f
Travis Patterson <masslessparticle@gmail.com>
2015-12-19 17:12:39 -0700

    Service Discovery

--- job/src/main/java/patterson/travis/job/JobApp.java
+++ job/src/main/java/patterson/travis/job/JobApp.java
@@ -2,6 +2,7 @@ package patterson.travis.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 @RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class JobApp {
 
