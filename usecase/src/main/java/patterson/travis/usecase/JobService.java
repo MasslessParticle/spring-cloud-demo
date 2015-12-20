2f87e11d12a398b2f844c2af11a6e9542cba2399
Travis Patterson <masslessparticle@gmail.com>
2015-12-19 17:44:27 -0700

    Circuit Breaker With Hystrix

--- usecase/src/main/java/patterson/travis/usecase/JobService.java
+++ usecase/src/main/java/patterson/travis/usecase/JobService.java
@@ -1,10 +1,13 @@
 package patterson.travis.usecase;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+
+import static java.util.Arrays.*;
 
 @Service
 public class JobService {
@@ -12,7 +15,12 @@ public class JobService {
     @Autowired
     private RestTemplate rest;
 
+    @HystrixCommand(fallbackMethod = "defaultJob")
     public List<String> jobs() {
         return (List<String>)rest.getForObject("http://jobs/", Object.class);
+    }
+
+    public List<String> defaultJob() {
+        return asList("Default Job");
     }
 }
--- usecase/src/main/java/patterson/travis/usecase/UsecaseApp.java
+++ usecase/src/main/java/patterson/travis/usecase/UsecaseApp.java
@@ -3,8 +3,10 @@ package patterson.travis.usecase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 @RestController
 @SpringBootApplication
 public class UsecaseApp {
