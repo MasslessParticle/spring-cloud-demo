dcf1601e738675e80805258afd200777fdb6ebf7
Travis Patterson <masslessparticle@gmail.com>
2015-12-19 17:11:12 -0700

    Config Server

---
+++ configserver/src/main/java/patterson/travis/configserver/ConfigserverApp.java
@@ -0,0 +1,14 @@
+package patterson.travis.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigserverApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserverApp.class, args);
+    }
+}
